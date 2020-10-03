6105bb056ef37c2e7827fc6c30fb2d52c2edb03e
Victor Banerjee <recervictory@gmail.com>
2020-10-03 19:33:33 +0530

    Java Array - Prime Factors

---
+++ 02_Array/ToDo.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class ToDo
+{
+   public static void main(String args[]){
+       
+       Scanner in = new Scanner(System.in);
+       
+       System.out.println("Enter the size of List");
+       int size = in.nextInt();
+       in.nextLine(); /// solve the line problem
+       
+       int pri[] = new int[size];
+       String plan[] =new String[size];
+        
+       int l = pri.length;
+       System.out.println("The length of array is "+l);
+       
+       System.out.println("Input: ");
+       for(int i = 0; i < size; i++){
+           System.out.println("Enter a plan: ");
+            plan[i] = in.nextLine();
+            System.out.println("Enter a priority: ");
+            pri[i] = in.nextInt();
+            in.nextLine(); //resolve line problem
+        }
+        
+       System.out.println("Output:");
+       for(int i = 0; i < l; i++){
+           System.out.println((i+1)+" "+plan[i] +"\t"+pri[i]);
+        }
+       
+    
+    } 
+}
