56d52f4d7feee35c3d62d967c13aaf55061dbfda
Victor Banerjee <recervictory@gmail.com>
2020-10-08 11:31:13 +0530

    09 - Nested Array - FooBar

--- 04_Hello Java/HelloJava.java
+++ 04_Hello Java/HelloJava.java
@@ -1,3 +1,7 @@
+/*
+Code: Implement HelloJava to print on the console
+Autor: Victor Banerjee
+*/
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello, Java");
---
+++ 09_Nested for Loops/FooBar.java
@@ -0,0 +1,31 @@
+/*
+Code: Implement FooBar
+Author: Victor Banerjee
+*/
+
+import java.util.*;
+
+public class FooBar {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n;
+        System.out.println("Enter a Number");
+        n = in.nextInt();
+        System.out.println("Loops Start");
+        // Loop start
+        for (int i = 1; i <= n; i++) {
+            // print the int values
+            if (i % 5 != 0 && i % 3 != 0) {
+                System.out.print(i + "\t");
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.print("Foo");
+            }
+            if (i % 5 == 0) {
+                System.out.print("Bar");
+            }
+            System.out.print("\t");
+        }
+    }
+}
