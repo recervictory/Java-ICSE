1845aa6c327fbeb2656b9df3bf01250dfff35a98
Victor Banerjee <recervictory@gmail.com>
2020-10-08 10:20:45 +0530

    Array Todo list

--- 02_Array/ToDo.java
+++ 02_Array/ToDo.java
@@ -1,34 +1,37 @@
 import java.util.*;
-public class ToDo
-{
-   public static void main(String args[]){
-       
-       Scanner in = new Scanner(System.in);
-       
-       System.out.println("Enter the size of List");
-       int size = in.nextInt();
-       in.nextLine(); /// solve the line problem
-       
-       int pri[] = new int[size];
-       String plan[] =new String[size];
-        
-       int l = pri.length;
-       System.out.println("The length of array is "+l);
-       
-       System.out.println("Input: ");
-       for(int i = 0; i < size; i++){
-           System.out.println("Enter a plan: ");
+/*
+ Program : Implement Todo List with Array
+ Author : Victor Banerjee
+*/
+
+public class ToDo {
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the size of List");
+        int size = in.nextInt();
+        in.nextLine(); /// solve the line problem
+
+        int pri[] = new int[size];
+        String plan[] = new String[size];
+
+        int l = pri.length;
+        System.out.println("The length of array is " + l);
+
+        System.out.println("Input: ");
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter a plan: ");
             plan[i] = in.nextLine();
             System.out.println("Enter a priority: ");
             pri[i] = in.nextInt();
-            in.nextLine(); //resolve line problem
+            in.nextLine(); // resolve line problem
         }
-        
-       System.out.println("Output:");
-       for(int i = 0; i < l; i++){
-           System.out.println((i+1)+" "+plan[i] +"\t"+pri[i]);
+
+        System.out.println("Output:");
+        for (int i = 0; i < l; i++) {
+            System.out.println((i + 1) + " " + plan[i] + "\t" + pri[i]);
         }
-       
-    
-    } 
+
+    }
 }
