ba630ccb920b28863e48b07d1a487317fd1b3c65
Victor Banerjee <recervictory@gmail.com>
2020-10-11 14:16:49 +0530

    ArrayList - StudentDB

--- 10_Array/StudentDb.java
+++ 10_Array/StudentDb.java
@@ -1,3 +1,4 @@
+// Inplement ArrayList
 import java.util.*;
 
 public class StudentDb {
@@ -11,22 +12,35 @@ public class StudentDb {
         System.out.print("Class: ");
         student[2] = in.nextLine();
         System.out.print("Marks: ");
-        student[3] = in.nextLine();
+        student[3] = in.next();
         in.close();
         return student;
     }
 
     public static void display_entry(String[] student) {
-        System.out.println("Name: " + student[0]);
-        System.out.println("Roll No: " + student[1]);
-        System.out.println("Class: " + student[2]);
-        System.out.println("Marks: " + student[3]);
+        System.out.println(
+                "Name: " + student[0] + " Roll No: " + student[1] + " Class: " + student[2] + " Marks: " + student[3]);
     }
 
     public static void main(String args[]) {
-        String[] student1 = new String[4];
-        student1 = insurt_entry();
-        display_entry(student1);
-
+        Scanner in = new Scanner(System.in);
+        char ch;
+        List<String[]> studentList = new ArrayList<String[]>();
+        //Insert
+        while (true) {
+            studentList.add(insurt_entry());
+            System.out.println("Do you wants to enter another student (Y/N): ");
+            ch = in.next().toCharArray()[0];
+            if (ch != 'y' && ch != 'Y')  {
+                break;
+            }
+            System.out.println("You entered " + ch);
+        }
+        
+        //Displaydisplay_entry
+        for (int i = 0; i < studentList.size(); i++) {
+            display_entry(studentList.get(i));
+        }
+        
     }
 }
